509b62d365e404d29a444aa0d465839caa3e8018
oatmealraisin <rymurphy@redhat.com>
2018-12-06 17:55:45 -0500

    Fixed some stuff w Viper

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,19 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/oatmealraisin/tasker/pkg/storage"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfg string
+var (
+	cfg string
+	db  storage.Storage
+)
 
 // TaskerCmd represents the base command when called without any subcommands
 var TaskerCmd = &cobra.Command{
@@ -14,8 +22,9 @@ var TaskerCmd = &cobra.Command{
 	Long:  ``,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the TaskerCmd.
+// Execute adds all child commands to the root command and sets flags
+// appropriately.  This is called by main.main(). It only needs to happen once
+// to the TaskerCmd.
 func Execute() error {
 	return TaskerCmd.Execute()
 }
@@ -24,21 +33,47 @@ func Execute() error {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	TaskerCmd.PersistentFlags().StringVarP(&cfg, "config", "c", "", "Config directory to use.")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	// The config name is always "config". May make this changeable
-	viper.SetConfigName("config")
 	if cfg != "" {
-		viper.AddConfigPath(cfg)
+		viper.SetConfigFile(cfg)
 	} else {
-		viper.AddConfigPath("$XDG_CONFIG_HOME/tasker")
-		viper.AddConfigPath("$HOME/.tasker")
-		viper.AddConfigPath("/etc/tasker")
+		viper.AddConfigPath("$XDG_CONFIG_HOME/tasker/")
+		viper.AddConfigPath("$HOME/.config/tasker/")
+		viper.AddConfigPath("/etc/tasker/")
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+	}
+
+	viper.SetDefault("StorageType", "sqlite")
+	viper.SetDefault("WorkingDir", "$XDG_DATA_HOME/tasker")
+	viper.SetDefault("Delimiter", "|")
+	viper.SetDefault("EditCmd", "vi")
+
+	viper.SetEnvPrefix("tasker")
+	viper.AutomaticEnv()
+	// This means that any config variable can be set using the corresponding
+	// TASKER_* environment variable
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Could not read config file: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	switch viper.GetString("StorageType") {
+	case "csv":
+		db = storage.NewCsvStorage(filepath.Join(viper.GetString("WorkingDir"), "tasklist.csv"))
+		break
+	case "sqlite":
+		panic("sqlite not implemented")
+	case "postgres":
+		panic("postgres not implemented")
+	default:
+		fmt.Printf("Unknown database type: %s", viper.GetString("StorageType"))
 	}
 }
